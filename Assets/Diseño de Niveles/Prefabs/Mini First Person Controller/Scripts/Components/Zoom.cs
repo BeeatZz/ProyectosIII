c9cec0d0f0c86765237d85af71fa2317ad26e802
V\V <beyondvicks@gmail.com>
2026-03-03 19:39:12 +0100

    Blocking del primer nivel (Zona del Ascensor)

---
+++ Assets/Diseño de Niveles/Prefabs/Mini First Person Controller/Scripts/Components/Zoom.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+[ExecuteInEditMode]
+public class Zoom : MonoBehaviour
+{
+    Camera cam;
+
+    public float defaultFOV = 60f;
+    public float maxZoomFOV = 15f;
+
+    [Range(0, 1)]
+    public float currentZoom;
+
+    public float sensitivity = 1f;
+
+    void Awake()
+    {
+        cam = GetComponent<Camera>();
+
+        if (cam != null)
+        {
+            defaultFOV = cam.fieldOfView;
+        }
+    }
+
+    void Update()
+    {
+        if (cam == null)
+            return;
+
+        if (Mouse.current != null)
+        {
+            float scroll = Mouse.current.scroll.ReadValue().y;
+
+            // Ajustado porque el nuevo sistema devuelve valores más grandes
+            currentZoom += scroll * sensitivity * 0.001f;
+            currentZoom = Mathf.Clamp01(currentZoom);
+
+            cam.fieldOfView = Mathf.Lerp(defaultFOV, maxZoomFOV, currentZoom);
+        }
+    }
+}
