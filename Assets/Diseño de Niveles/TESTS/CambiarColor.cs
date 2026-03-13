005d2ddb7390e7a7d9b25679b926a5fbdc409861
JaimeSevilla <jaimesevillamoreno1@gmail.com>
2026-03-13 10:31:28 +0100

    Creación del script de cambio de color Inicialización del mismo

---
+++ Assets/Diseño de Niveles/TESTS/CambiarColor.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CambiarColor : MonoBehaviour
+{
+    public GameObject cambio1;
+    public GameObject cambio2;
+    public Material materialcolor;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
