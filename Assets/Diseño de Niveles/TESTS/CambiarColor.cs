3b435f8c939269a3a03f0a88177d64df4c0c1146
JaimeSevilla <jaimesevillamoreno1@gmail.com>
2026-03-14 00:12:22 +0100

    Añadido de la escena de accesibilidad para pruebas Añadido de materiales Añadido del script de cambio de color y pruebas Correcto funcionamiento en objetos y en textos y fondos

--- Assets/Diseño de Niveles/TESTS/CambiarColor.cs
+++ Assets/Diseño de Niveles/TESTS/CambiarColor.cs
@@ -1,22 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class CambiarColor : MonoBehaviour
 {
     public GameObject cambio1;
     public GameObject cambio2;
-    public Material materialcolor;
+    public Material materialdefecto;
+    public Material materialcambio;
+    private Color colorInicioCambio1;
+    private Color colorInicioCambio2;
+    private Color colorInicioBoton;
+    private Color colorInicioBotonFondo;
+    private bool ColorInicio = false;
+    public Button botonTexto;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    private void Start()
     {
-        
+        // grabar colores por defecto
+        colorInicioCambio1 = cambio1.GetComponent<MeshRenderer>().material.color;
+        colorInicioCambio2 = cambio2.GetComponent<MeshRenderer>().material.color;
+        colorInicioBoton = botonTexto.GetComponentInChildren<TextMeshProUGUI>().color;
+        colorInicioBotonFondo = botonTexto.image.color;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void cambiarColor()
     {
-        
+        if(ColorInicio == false)
+        {
+            cambio1.GetComponent<MeshRenderer>().material.color = materialdefecto.color;
+            cambio2.GetComponent<MeshRenderer>().material.color = materialcambio.color;
+            botonTexto.image.color = materialcambio.color;
+            botonTexto.GetComponentInChildren<TextMeshProUGUI>().color = Color.green;
+            ColorInicio = true;
+        }
+        else
+        {
+            cambio1.GetComponent<MeshRenderer>().material.color = colorInicioCambio1;
+            cambio2.GetComponent<MeshRenderer>().material.color = colorInicioCambio2;
+            botonTexto.image.color = colorInicioBotonFondo;
+            botonTexto.GetComponentInChildren<TextMeshProUGUI>().color = colorInicioBoton;
+            ColorInicio = false;
+        }
+
     }
+
 }
